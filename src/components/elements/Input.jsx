c408b6d729db2f678dec27b22d126af0570845d1
Pitamber-Dhungel <sumandhungel9@gmail.com>
2026-03-16 17:50:41 +0545

    Correct Style In  Input Component

--- src/components/elements/Input.jsx
+++ src/components/elements/Input.jsx
@@ -6,8 +6,8 @@ const Input = ({ type, id, name,placeholder, className=""}) => {
         id={id}
         name={name}
         placeholder={placeholder}
-        className={`w-full px-2 py-4 rounded-sm border border-[#C8CED9] text-sm text-[#5B6B86] outline-none font-normal transition-all duration-200
-            focus:ring-2 focus:ring-[#0069A8]/30 focus:border-[#0069A8] ${className}`}
+        className={`w-full px-2 py-4 rounded-sm border border-[#C8CED9] text-sm text-[#5B6B86] placeholder-[#5B6B86] outline-none font-normal transition-all duration-200
+         focus:border-[#0069A8] placeholder:text-sm ${className}`}
       />
     </>
   );
