43a6265dfa8d181f78cdb2fd563139a01fbe8c44
Deepankarshakya <143170727+Deepankarshakya@users.noreply.github.com>
2026-03-17 00:14:18 +0545

    add logo component

---
+++ src/components/elements/Logo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Logo = () =>{
+    return(
+    <div className="bg-white px-4 py-2 rounded font-semibold text-amber-800 inline-block">
+        Glam Beauty
+    </div>
+    );
+};
+
+export default Logo;
