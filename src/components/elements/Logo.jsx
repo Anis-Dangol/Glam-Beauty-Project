14ecd16276af53ff26b1e1ebad1df1a9708e979c
Deepankarshakya <143170727+Deepankarshakya@users.noreply.github.com>
2026-03-17 15:06:24 +0545

    Logo with Link

--- src/components/elements/Logo.jsx
+++ src/components/elements/Logo.jsx
@@ -2,11 +2,9 @@ import { Link } from 'react-router-dom'
 
 const Logo = () => {
     return (
-    
     <Link to="/" className="font-bold text-[20px] text-[#5B4636] py-4">
         Glam Beauty
     </Link>
-    
     );
 };
 
