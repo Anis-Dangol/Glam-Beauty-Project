c8eaf6af229c063eeddd1e03b6b8e2c9b9dd91d1
Anish Dangol <anis.dangol121@gmail.com>
2026-03-17 17:39:18 +0545

    changes in prop in logo components

--- src/components/elements/Logo.jsx
+++ src/components/elements/Logo.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-
-const Logo = ({className}) =>{
-    return(
-    <div className={`bg-white py-[16px] text-[#5B4636] text-[20px] font-bold ${className}`}>
-        Glam Beauty
+const Logo = ({ background, size, className }) => {
+  return (
+    <div
+      className={`bg-${background} py-4 text-[#5B4636] text-${size} font-bold ${className}`}
+    >
+      Glam Beauty
     </div>
-    );
+  );
 };
 
 export default Logo;
