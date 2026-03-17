4f527a61eeee5cc7db792650f9bbbf9a4c3a2418
Deepankarshakya <143170727+Deepankarshakya@users.noreply.github.com>
2026-03-17 14:54:13 +0545

    Logo with link

--- src/components/elements/Logo.jsx
+++ src/components/elements/Logo.jsx
@@ -2,12 +2,12 @@ import { Link } from 'react-router-dom'
 
 const Logo = () => {
     return (
-    <div className="flex items-center p-4">
-    <Link to="/" className="font-bold text-[20px] text-[#5B4636]">
+    
+    <Link to="/" className="font-bold text-[20px] text-[#5B4636] py-4">
         Glam Beauty
     </Link>
-    </div>
-    )
-}
+    
+    );
+};
 
-export default Logo
+export default Logo;
