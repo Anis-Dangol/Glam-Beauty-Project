9880158031ad2e99ff1290401f9b96d9bc11a5c9
Deepankarshakya <143170727+Deepankarshakya@users.noreply.github.com>
2026-03-17 10:58:40 +0545

    add logo component

--- src/components/elements/Logo.jsx
+++ src/components/elements/Logo.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Logo = () =>{
     return(
-    <div className="bg-white pl-20 py-10 rounded text-[#5B4636] inline-block text-2xl font-bold">
+    <div className="bg-white py-[16px] px-[8px] w-[150px] h-[20px] rounded text-[#5B4636] inline-block text-[20px] font-bold">
         Glam Beauty
     </div>
     );
