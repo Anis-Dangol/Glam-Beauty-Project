fb9acfbb2715fb1381339e33870b9e5b8ff0b4f9
Pitamber-Dhungel <sumandhungel9@gmail.com>
2026-03-22 15:33:49 +0545

    Add protected route to handle security and edit necessary component

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,36 @@
+
+import { useState } from "react";
+import { AuthContext } from "./AuthContext";
+import { getCurrentUser, logoutUser } from "../services/userService";
+import { getData } from "../services/storage";
+
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(() => {
+    return getCurrentUser();
+  });
+
+  const login = async (data) => {
+    const users = getData("users");
+
+    const user = users.find(
+      (u) =>
+        u.email === data.email.toLowerCase().trim() &&
+        u.password === data.password
+    );
+    return user
+  };
+
+  const logout = () => {
+    logoutUser();
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import { createRoot } from "react-dom/client";
 import "../src/styles/index.css";
 
 import Router from "./routes/Routes.jsx";
+import AuthProvider from "./context/AuthProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Router />
+    <AuthProvider>
+      <Router />
+    </AuthProvider>
   </StrictMode>,
 );
