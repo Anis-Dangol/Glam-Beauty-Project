2546162059b6e38d8dd89fddda9b74b21c7219e4
Anish Dangol <anis.dangol121@gmail.com>
2026-03-23 12:32:53 +0545

    added add to cart in product details

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -1,23 +1,31 @@
 import { getData, setData } from "./storage";
 
-
 export const getCartByUser = (userId) => {
   const cart = getData("cart");
-  return cart.find(c => c.userId === userId) || { userId, items: [] };
+  return cart.find((c) => c.userId === userId) || { userId, items: [] };
 };
 
+export const checkCart = (userId, productId) => {
+  const cart = getData("cart");
+  const currentCart = cart.find((c) => c.userId === userId);
+
+  const productFound = currentCart
+    ? currentCart.items.find((p) => p.productId === productId)
+    : false;
+
+  return productFound || false;
+};
 export const addToCart = (userId, productId, qty = 1) => {
   let cart = getData("cart");
 
-  let userCart = cart.find(c => c.userId === userId);
+  let userCart = cart.find((c) => c.userId === userId);
 
   if (!userCart) {
     userCart = { userId, items: [] };
     cart.push(userCart);
   }
-  
 
-  const existing = userCart.items.find(i => i.productId === productId);
+  const existing = userCart.items.find((i) => i.productId === productId);
 
   if (existing) {
     existing.quantity += qty;
@@ -31,13 +39,13 @@ export const addToCart = (userId, productId, qty = 1) => {
 export const removeFromCart = (userId, productId) => {
   let cart = getData("cart");
 
-  cart = cart.map(c =>
+  cart = cart.map((c) =>
     c.userId === userId
       ? {
           ...c,
-          items: c.items.filter(i => i.productId !== productId)
+          items: c.items.filter((i) => i.productId !== productId),
         }
-      : c
+      : c,
   );
 
   setData("cart", cart);
@@ -46,9 +54,7 @@ export const removeFromCart = (userId, productId) => {
 export const clearCart = (userId) => {
   let cart = getData("cart");
 
-  cart = cart.map(c =>
-    c.userId === userId ? { ...c, items: [] } : c
-  );
+  cart = cart.map((c) => (c.userId === userId ? { ...c, items: [] } : c));
 
   setData("cart", cart);
 };
